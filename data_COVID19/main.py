3e25eb12bf663c3b1102f40f19d30ce9d0b9fdc1
Julia <juliadanielesilva@outlook.com>
2021-07-13 11:01:09 -0300

    Corrections to the final data

--- data_COVID19/main.py
+++ data_COVID19/main.py
@@ -1,6 +1,4 @@
 import pandas as pd
-from datetime import datetime
-from AnnualVariation import AnnualVarition
 
 data = pd.read_excel("./files/owid-covid-data.xlsx")
 
@@ -24,9 +22,31 @@ dataBRA = dataBRA.dropna()
 dataBRA.to_csv("./files/dataBRA.csv", index=False)
 
 # Calculation of annual variation
-AnnualVarition()
+datas = []
+mortesTotal = []
+mortesDia = []
+
+dataInicial = int(dataBRA[dataBRA['Data'].str.contains('17/03/2021')].index.values)
+dataFinal = int(dataBRA.iloc[[-1]].index.values)
+dataComparacao = int(dataBRA[dataBRA['Data'].str.contains('17/03/2020')].index.values)
+
+while dataInicial <= dataFinal:
+    dado_ano_atual = dataBRA.loc[[dataInicial]]
+    dado_ano_anterior = dataBRA.loc[[dataComparacao]]
+
+    datas.append(dado_ano_atual['Data'].values)
+    mortesTotal.append(
+        ((dado_ano_atual['Total de Morte'].values / dado_ano_anterior['Total de Morte'].values) - 1) * 100)
+    mortesDia.append(
+        ((dado_ano_atual['Novas Mortes no Dia'].values / dado_ano_anterior['Novas Mortes no Dia'].values) - 1) * 100)
+
+    dataInicial += 1
+    dataComparacao += 1
+
 dataAnual = pd.DataFrame()
-dataAnual['Data'] = AnnualVarition.datas
-dataAnual['Total de Mortes'] = AnnualVarition.mortesTotal
-dataAnual['Novas Mortes no Dia'] = AnnualVarition.mortesDia
+dataAnual['Data'] = datas
+dataAnual['Total de Mortes'] = mortesTotal
+dataAnual['Novas Mortes no Dia'] = mortesDia
 dataAnual.to_csv("./files/dataAnual.csv",  index=False)
+
+print('Fim das execução')
